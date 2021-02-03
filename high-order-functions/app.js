7f7102ce6d388cd023c6391842fe0cb7a98e3899
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-02-03 08:07:21 +0400

    Initial version

---
+++ high-order-functions/app.js
@@ -0,0 +1,26 @@
+/*
+You should make request on the following URL: 
+https://next.jsongenerator.com/api/json/get/VkBw8XP2d 
+Get the JSON data and perform some operations on the data.
+You must use high order functions such as: filter, map, reduce, forEach, etc…
+*/
+
+// 1. Find and print average salary of all employees who works in Georgia, Amazon company.
+
+// 2. Find and print list of the employees which work in Facebook IT department.
+
+// 3. Find and print boolean value if there exists employees which works in India HR department and whose salary is above 500 000
+
+// 4. Get and print a list of employees with only email, firstname and dob of the employees which work in Google UK Sales department.
+
+// 5. Find and print an employee which has highest salary among those employees which work in either Google or Apple and are born after 1980.
+
+// 6. Find an average age of all employees and compare if to average age of Google employees. Print "1" if average age of all employees is smaller, otherwise print "2".
+
+// 7. Sort and print employees in alphabetical order by firstname
+
+// 8. Check if there exists and employee in Google which has salary over 600 000 and is born after 1990.
+
+// 9. Find and print sum of salaries of the users which work in USA Apple.
+
+// 10. Find and print list of all employees, which has birth hours in between 00:00AM and 12:00PM
